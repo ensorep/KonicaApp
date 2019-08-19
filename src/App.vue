e0e6064cc3f4fa89d7897f6e8a06595f94962de5
Reggie Peterson <tao.rep@gmail.com>
2019-08-19 02:30:35 -0400

    initial build

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Search/>
+    <div class="movies">
+      <Movie v-for="movie in movies"
+    :key="movie.id" :title="movie.title"
+    :poster="movie.poster_path" :overview="movie.overview"
+    :release="movie.release_date" :vote_average="movie.vote_average"
+    />
+     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Movie from "./components/Movie.vue";
+import Search from "./components/Search.vue"
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Search,
+    Movie
+  },
+  data(){
+    return{
+      movies: []
+    }
+  },
+  created(){
+      const data = fetch('https://api.themoviedb.org/3/discover/movie?primary_release_year=2019/api_key=9084eae9f770e006ebcba95dbd474e28')
+      data.then(response=>response.json()).then(data=>this.movies = data.results)
   }
 };
 </script>
@@ -24,5 +41,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.movies {
+  display:flex;
+  width: 100%;
+  flex-wrap: wrap;
+  align-items: flex-start;
 }
 </style>
---
+++ src/components/Movie.vue
@@ -0,0 +1,57 @@
+<template>
+<div class="movie">
+    <div class="movie-title">{{title}}</div>
+    <img class="movie_poster" :src="imgpath + poster"  />
+    <div class="movie-release">{{release}}</div>
+    <div class="movie-overview">{{overview}}</div>
+    <div class="movie-voteAvg">Average rating: {{vote_average}} / 10</div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Movie',
+    props: {
+        title: String,
+        overview: String,
+        release: String,
+        vote_average: Number,
+        poster: String,
+    },
+    computed:{
+       
+    },
+    data() {
+        return {
+            imgpath:`https://image.tmdb.org/t/p/w500`
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+ .movie {
+     display: flex;
+     flex: 1;
+     flex-direction: column;
+     justify-content: space-evenly;
+     margin: 1rem;
+     max-width: 360px;
+    &_poster {
+        height: 400px;
+    }
+    &-overview {
+        margin: auto;
+        text-align: left;
+    }
+    &-title {
+        margin: 1rem 0;
+    }
+    &-release {
+        margin: 1rem 0;
+    }
+    &-voteAvg {
+        margin: 1rem;
+    }
+ }
+</style>
