4990d52fda22a718174d9cd7aeed09f12e1f04c4
Reggie Peterson <tao.rep@gmail.com>
2019-08-19 16:09:14 -0400

    update card styling

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,10 +1,12 @@
 <template>
 <div class="movie">
-    <div class="movie-title">{{title}}</div>
     <img class="movie_poster" :src="imgpath + poster"  />
-    <div class="movie-release">{{release}}</div>
-    <div class="movie-overview">{{overview}}</div>
-    <div class="movie-voteAvg">Average rating: {{vote_average}} / 10</div>
+    <div class="movie_info">
+        <div class="movie_info-title">{{title}}</div>
+        <div class="movie_info-release">{{release}}</div>
+        <div class="movie_info-overview">{{overview}}</div>
+        <div class="movie_info-voteAvg">Average rating: {{vote_average}} / 10</div>
+    </div>
 </div>
 </template>
 
@@ -31,29 +33,48 @@ export default {
 
 <style lang="scss" scoped>
  .movie {
-     background-color: #e2e2e2;
-     border-radius: 3px;
-     display: flex;
-     flex: 1;
-     flex-direction: column;
-     justify-content: space-evenly;
-     margin: 1rem;
-     max-width: 330px;
-    &_poster {
-        height: 350px;
-    }
-    &-overview {
+    background-color: #e2e2e2;
+    position: relative;
+    border-radius: 3px;
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    justify-content: space-evenly;
+    margin: 1rem;
+    max-width: 330px;
+  &_poster {
+    height: 350px;
+  }
+    &_info {
+      display: flex;
+      flex-direction: column;
+      color: #e2e2e2;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      background-color: rgba(0,0,0,0.6);
+      opacity: 0;
+
+      &:hover {
+        cursor: pointer;
+        opacity: 1;
+      }
+
+      &-overview {
         text-align: left;
         margin: 1rem;
-    }
-    &-title {
+      }
+      &-title {
+        margin: 1rem .5rem;
+      }
+      &-release {
         margin: 1rem 0;
-    }
-    &-release {
-        margin: 1rem 0;
-    }
-    &-voteAvg {
+      }
+      &-voteAvg {
         margin: 1rem;
+      }
     }
  }
 </style>
